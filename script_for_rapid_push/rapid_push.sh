e1df29e51ecfd7c07e5632132776e55d1f939003
Luis Hima <lhima@c1r3p1.42firenze.it>
2025-02-04 09:30:37 +0100

    rapid push

---
+++ script_for_rapid_push/rapid_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                     
+
+# Move into the specified directory                                             
+cd ../lavori42
+
+# Add all changes to the staging area                                           
+git add *
+
+# Commit the changes with the message "rapid push"                              
+git commit -m "rapid push"
+
+# Push the changes to the remote repository                                     
+git push
+
