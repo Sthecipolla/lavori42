cf129377ef4a8cc8cdfea1ef6943540a3c99f8c2
luis hima <himal8438@gmail.com>
2025-03-02 16:13:49 +0100

    rapid push

---
+++ philosopher/philo/philo.h
@@ -0,0 +1,21 @@
+# ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+/* typedef struct s_philo
+{
+	int				id;
+	int				left_fork;
+	int				right_fork;
+	int				eat_count;
+	long long		last_eat;
+	pthread_t		thread;
+	struct s_data	*data;
+}					t_philo;
+ */
+# endif
